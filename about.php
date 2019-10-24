9d7091951f7f1e5252a782ba7c4b739c28a7d925
Claire Mabbett <claire.mabbett@devopsgroup.com>
2019-10-24 16:35:19 +0100

    Adding images folder. Put background image in header. Change page background colour.

--- about.php
+++ about.php
@@ -37,7 +37,7 @@
 		<div class="row text-center">
 			<div class="col-12">
 				<h1>Improve Your Production Readiness</h1>
-				<p class="lead">This online Production Readiness Review will help you understand your current strengths and weaknesses and then recommend resources that can support you in taking the next steps on your Production Readiness journey.</p>
+				<p class="lead pb-4">This online Production Readiness Review will help you understand your current strengths and weaknesses and then recommend resources that can support you in taking the next steps on your Production Readiness journey.</p>
 				<a href="<?='section-' . SectionNameToURLName($survey->sections[0]['SectionName'])?>" class="btn btn-primary mr-2">Start the Questionnaire</a>
 				<a href="https://github.com/devopsguys/operational-readiness-review" target="_blank" class="btn btn-outline-light">Fork us on GitHub</a>
 			</div>
