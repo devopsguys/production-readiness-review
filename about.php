63b76cb803fcc301109c10afac1ae3bd44778b6c
Claire Mabbett <claire.mabbett@devopsgroup.com>
2019-10-28 14:07:14 +0000

    relinking headshots

--- about.php
+++ about.php
@@ -103,7 +103,7 @@
 				<div class="card mb-3">
 					<div class="row no-gutters">
 						<div class="col-md-4">
-							<img src="images/team-photos/devopsguys_team_steve_thair_004_low_res-square.jpg" class="card-img" alt="Steve Thair Headshot">
+							<img src="team-photos/devopsguys_team_steve_thair_004_low_res-square.jpg" class="card-img" alt="Steve Thair Headshot">
 						</div>
 						<div class="col-md-8 d-flex align-self-center">
 							<div class="card-body">
@@ -121,7 +121,7 @@
 				<div class="card mb-3">
 					<div class="row no-gutters">
 						<div class="col-md-4">
-							<img src="images/team-photos/RichardSweeney.png" class="card-img" alt="Richard Sweeney Headshot">
+							<img src="team-photos/RichardSweeney.png" class="card-img" alt="Richard Sweeney Headshot">
 						</div>
 						<div class="col-md-8 d-flex align-self-center">
 							<div class="card-body">
