36b2264cd502d7c08bd049908619ade066a2347b
Claire Mabbett <claire.mabbett@devopsgroup.com>
2019-11-01 15:15:03 +0000

    Removing commented out code

--- viewResources.php
+++ viewResources.php
@@ -21,46 +21,6 @@
 		</div>
 	</header>
 
-<!--
-	<div class="container">
-		<div class="row mb-5">
-			<div class="pt-0 pb-4 rounded text-left mx-auto">
-				<div class="bg-light rounded pl-2 pl-sm-4 pr-2 pr-sm-4 pb-2 pb-sm-4 border-primary border ml-sm-2 ml-xs-2 mb-2 mr-sm-2 mr-xs-2">
-						
-						<?php
-						
-						foreach ($advice as $adviceIndex=>$adviceSection)
-						{	
-							if ( $adviceIndex != '//' ) {
-						?>
-						
-						<div class="row">
-							<div class="col-lg-12 mt-2 mt-sm-4">
-								<div class="card border-primary">
-									<h5 class="card-header text-center text-white bg-primary">
-										<?=$adviceIndex?>
-									</h5>
-									<div class="card-body p-1">
-										<?php RenderAdvice($adviceIndex, false) ?>
-									</div>
-								</div>
-							</div>
-						
-						
-						</div>
-						
-						<?php 
-							}
-						} ?>
-				</div>
-			</div>
-
-			
-		
-		</div>
-		
-	</div>
--->
 
 	<div class="container">
 <!-- Three columns of text below the header  -->
