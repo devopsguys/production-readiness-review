a36726e6f8a799144cb40924af3f2e9e108fe5ad
Claire Mabbett <claire.mabbett@devopsgroup.com>
2019-11-05 12:15:12 +0000

    restructuring page

--- collectResponses.php
+++ collectResponses.php
@@ -6,90 +6,101 @@
 	$activePage = 'Questionnaire';
 	require 'header.php';
 ?> 
-
-<!-- Header -->
-	<header class="container-fluid">
-		<form action="test.php" method="POST">
+ 
+<form action="test.php" method="POST">
   
-			<?php
-
-				// Determine which section of the assessment we are showing
-				$uri = $_SERVER["REQUEST_URI"];
-				$currentSection = explode("section-", $uri)[1];
-
-				// Find the current section in our model
-				$sectionIndex = 0;
-				foreach ($survey->sections as $index=>$section)
-				{
-					if ( SectionnameToURLName($section['SectionName']) == $currentSection )
-					{
-						$sectionIndex = $index;
-					}
-				}
-
-				// Determine the URL names for the next and previous sections
-				$nextSection = '';
-				if ( $sectionIndex<sizeof($survey->sections)-1 )
-				{
-					$nextSection = SectionnameToURLName($survey->sections[$sectionIndex + 1]['SectionName']);
-				}
-				$previousSection = '';
-				if ( $sectionIndex>0 )
-				{
-					$previousSection = SectionnameToURLName($survey->sections[$sectionIndex - 1]['SectionName']);
-				}
-			?>
-
-				<div class="row form-group text-center">
-					<div class="col-12">
-						<h1><?=$survey->sections[$sectionIndex]['SectionName']?></h1>
-
-
-			<?php	
-				// Render all the question for the current section
-				foreach ($survey->sections[$sectionIndex]['Questions'] as $index=>$question)
-				{	
-					renderQuestion($question, $index);	
-				}
-				?>
-
-					</div>
-
-				</div>
-	
-			<div class="row form-group">
-				<div class="text-center col-lg-12">
-					<div class="btn-group btn-group-justified">
-						<?php if ($previousSection != '') { ?>
-						<div class="btn-group">
-							<button type="submit" class="btn btn-primary" onclick="$('form').attr('action', 'section-<?=$previousSection?>');">Previous</button>
-						</div>
-						<?php } ?>
-						<?php if ($nextSection != '') { ?>
-						<div class="btn-group" role="group">
-							<button type="submit" class="btn btn-primary" onclick="$('form').attr('action', 'section-<?=$nextSection?>');">Next</button>
-						</div>
-						<?php } ?>
-					</div>
-					<!-- Show results button if we are on the final section -->
-					<?php if ($nextSection == '') { ?>
-						<button type="submit" class="btn btn-primary" onclick="$('form').attr('action', 'results');">View Results</button>
-					<?php } ?>
-				</div>
-			</div>
-
-		</form>
-	</header>
-
 <?php
+
+	// Determine which section of the assessment we are showing
+	$uri = $_SERVER["REQUEST_URI"];
+	$currentSection = explode("section-", $uri)[1];
+	
+	// Find the current section in our model
+	$sectionIndex = 0;
+	foreach ($survey->sections as $index=>$section)
+	{
+		if ( SectionnameToURLName($section['SectionName']) == $currentSection )
+		{
+			$sectionIndex = $index;
+		}
+	}
+	
+	// Determine the URL names for the next and previous sections
+	$nextSection = '';
+	if ( $sectionIndex<sizeof($survey->sections)-1 )
+	{
+		$nextSection = SectionnameToURLName($survey->sections[$sectionIndex + 1]['SectionName']);
+	}
+	$previousSection = '';
+	if ( $sectionIndex>0 )
+	{
+		$previousSection = SectionnameToURLName($survey->sections[$sectionIndex - 1]['SectionName']);
+	}
+?>
+
+	
+	<!-- Header -->
+	<header class="container-fluid">
+		<div class="row text-center">
+			<div class="col-12">
+				<h1><?=$survey->sections[$sectionIndex]['SectionName']?></h1>
+			</div>
+		</div>
+	</header>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<?php	
+		// Render all the question for the current section
+		foreach ($survey->sections[$sectionIndex]['Questions'] as $index=>$question)
+		{	
+			renderQuestion($question, $index);	
+		}
+		?>
+			</div>
+		</div>
+	</div>
+	
+	
+	<!-- Next and previous buttons -->	
+		
+	<div class="row form-group">
+	<div class="text-center col-lg-12">
+		<div class="btn-group btn-group-justified">
+			<?php if ($previousSection != '') { ?>
+			<div class="btn-group">
+				<button type="submit" class="btn btn-primary" onclick="$('form').attr('action', 'section-<?=$previousSection?>');">Previous</button>
+			</div>
+			<?php } ?>
+			<?php if ($nextSection != '') { ?>
+			<div class="btn-group" role="group">
+				<button type="submit" class="btn btn-primary" onclick="$('form').attr('action', 'section-<?=$nextSection?>');">Next</button>
+			</div>
+			<?php } ?>
+		</div>
+		<!-- Show results button if we are on the final section -->
+		<?php if ($nextSection == '') { ?>
+			<button type="submit" class="btn btn-primary" onclick="$('form').attr('action', 'results');">View Results</button>
+		<?php } ?>
+	</div>
+	</div>
+	
+</form>
+	
+	<?php
 	
 	require 'footer.php';
-	
+
+	// Function for adding questions
+
 	function renderQuestion($question) { 
 		
 		?>
-		
-				<div class="card mt-4 ml-sm-2 ml-xs-0 mr-sm-2 mr-xs-0 text-left shadow">
+	
+<!-- Question Card -->
+
+				<div class="card text-left shadow">
 					<?php if ($question['Type']!='Banner') {?>
 					<h6 class="card-header"><?=$question['QuestionText']?></h6>
 					<?php } ?>
@@ -107,13 +118,13 @@
 									break;
 							} ?>
 		
+						</div>
 					</div>
-				</div>
 				
 		
 		<?php
 	}
-
+	
 	function renderCheckboxes($question) { 
 		
 		// We render a hidden field for each checkbox to enable us to recognise if chckboxes have been unchecked
@@ -139,4 +150,5 @@
 	
 ?>	
 
+	
 
